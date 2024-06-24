00e984ae1ba8bb229698eb71ed26707d53ba899c
aladoui <125268611+aladoui@users.noreply.github.com>
2024-06-24 16:19:59 +0100

    add german selectors

--- src/shared/selectors.ts
+++ src/shared/selectors.ts
@@ -4,11 +4,12 @@ export const SELECTORS = {
     searchButton: 'input[id*="search"][type="submit"]',
     priceInput: 'input[id*="upper-bound"][type="range"]',
     priceInputHidden: "input[name*='high-price'][type='hidden']",
-    priceButton: 'input[aria-label*="Go"][type="submit"]',
+    priceButton:
+      'input[aria-label*="Go"][type="submit"], input[aria-label*="Start – Preisspanne einreichen"][type="submit"]',
     xResults:
-      "//*[contains(text(), 'Results') and not(.//*[contains(text(), 'Results')])]",
+      "//*[contains(text(), 'Results') and not(.//*[contains(text(), 'Results')])] | //*[contains(text(), 'Ergebnissen') and not(.//*[contains(text(), 'Ergebnissen')])]",
     xNoResults:
-      "//*[contains(text(), 'No results') and not(.//*[contains(text(), 'No results')])]",
+      "//*[contains(text(), 'No results') and not(.//*[contains(text(), 'No results')])] | //*[contains(text(), 'Keine Ergebnisse') and not(.//*[contains(text(), 'Keine Ergebnisse')])]",
   },
   ebay: {
     searchInput: 'input[aria-label*="Search"]',
